fd96a2282f1209a9f866160ac0a8bbb74b42728d
Jamiecode <ultratude.mobile@gmail.com>
2020-07-25 12:14:53 +0300

    eventlet

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from deliverywebapp import app
+import eventlet
+import eventlet.wsgi
 
 
 if __name__ == '__main__':
-    app.run(port=9001)
+    # app.run(port=9001)
+    eventlet.wsgi.server(eventlet.listen(('', 9001)), app)
 
 
